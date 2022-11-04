a75689afb376c50b9d79d525cc211d53c120479d
ProBm0203 <bhaskarmishra12675@gmail.com>
2022-11-05 00:08:41 +0530

    changes done

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 8000;
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static'))//for serving static files
+app.use(express.urlencoded());
+
+
+//PUG SPECIFIC STUFF
+app.set('view engine', 'pug')//set the template engine as pug
+app.set('views',path.join(__dirname, 'views'))//set the views directory
+
+//ENDPOINTS
+
+app.get('/',(req, res)=>{
+    const params = {};
+    res.status(200).render('index.pug',  params);
+})
+
+//START THE SERVER 
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+})
