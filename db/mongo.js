8cb5917c52cc442be976dff94a7dab7765e8766a
Mohit kumar <monty11113333@gmail.com>
2022-11-08 22:35:37 +0530

    adding data to database

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,6 @@
 const mongoose  = require('mongoose');
 
-mongoose.connect('mongodb://localhost/DATA',{
+mongoose.connect('mongodb://localhost/CONTACT',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then (() =>{
