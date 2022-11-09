cab401e8445430e9ad778ebf8b0703830e25cbd0
Mohit kumar <monty11113333@gmail.com>
2022-11-09 15:06:04 +0530

    Adding HamBurger button and styling

--- db/user.js
+++ db/user.js
@@ -8,13 +8,11 @@ const userSchema = new mongoose.Schema({
     email :{
         type: String,
         required: true,
-        unique : true
-
+        
     },
     phone : {
         type : String,
         required : true,
-        unique : true
     },
     message : {
         type: String,
